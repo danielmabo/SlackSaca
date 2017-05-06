e49a42796790a0ee9261dee7aca5b1752bace296
Gabibaskes <javivaz1996@gmail.com>
2017-05-06 20:50:57 +0200

    Models

--- slacksaca_django/SlackSaca_App/models.py
+++ slacksaca_django/SlackSaca_App/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.utils import timezone #puede no ser necesario
 class Team(models.Model):
     name = models.CharField(max_length=200)
     team_id = models.CharField(max_length=20)
     bot_user_id = models.CharField(max_length=20)
     bot_access_token = models.CharField(max_length=100)
+
+class User(models.Model):
+    user_name = models.CharField(max_length=100)
+    knows = models.Charfield(max_length=200)
+    in_team = models.ForeignKey('Team') #Referencia a qué equipo pertenece (?)
+
+class Question(models.Model):
+    question = models.CharField(max_length=400) #La pregunta
+    subject = models.CharField(max_length=15) #Sobre qué tema (lenguaje)
+    asked = models.ForeignKey('User') #Quién ha preguntado
