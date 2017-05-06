4361e51191d21363f415d72f8f5136e0686c9cb1
Daniel Martinez Bordes <dmartinezbordes@gmail.com>
2017-05-06 18:09:29 +0200

    Django app v0.0.1

---
+++ slacksaca_django/SlackSaca_App/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SlacksacaAppConfig(AppConfig):
+    name = 'SlackSaca_App'
---
+++ slacksaca_django/SlackSaca_App/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.conf import settings
+
+import json
+
+def index(request):
+    client_id = settings.SLACK_CLIENT_ID
+    return render(request, 'templates/landing.html', {'client_id': client_id})
+
+
+def slack_oauth(request):
+    code = request.GET.get('code')
+
+    params = {
+        'code': code,
+        'client_id': settings.SLACK_CLIENT_ID,
+        'client_secret': settings.SLACK_CLIENT_SECRET
+    }
+    url = 'https://slack.com/api/oauth.access'
+    json_response = request.GET.get(url, params)
+    data = json.loads(json_response)
+    Team.objects.create(
+        name=data['name'],
+        team_id=data['team_id'],
+        bot_user_id=data['bot']['bot_user_id'],
+        bot_access_token=data['bot']['bot_access_token']
+    )
+    return HttpResponse('Bot added to your Slack team!')
